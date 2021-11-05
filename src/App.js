61ce2a49cf35cd0b04d4c1385a2440b56b6b0e40
vikhulei <65869915+vikhulei@users.noreply.github.com>
2021-11-05 20:23:35 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Events from "./pages/events";
+import AnnualReport from "./pages/annual";
+import Teams from "./pages/team";
+import Blogs from "./pages/blogs";
+import SignUp from "./pages/signup";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/annual" element={<AnnualReport />} />
+        <Route path="/team" element={<Teams />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
